6aa96e9e9e8707e89f511502a080aa01105730a4
Samad Arshad <samad.arshad@ev.energy>
2022-05-11 08:14:35 +0100

    Create Tables

--- sql_queries.py
+++ sql_queries.py
@@ -1,31 +1,77 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id INT PRIMARY KEY,
+    start_time TIMESTAMP NOT NULL,
+    user_id INT NOT NULL,
+    level VARCHAR(4),
+    song_id VARCHAR(18),
+    artist_id VARCHAR(18),
+    session_id INT,
+    location VARCHAR(128),
+    user_agent VARCHAR(256),
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (song_id) REFERENCES songs (song_id),
+    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+    );
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR(128),
+    last_name VARCHAR(128),
+    level VARCHAR(4),
+    gender VARCHAR(1)
+    );
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id VARCHAR(18) PRIMARY KEY,
+    title VARCHAR(128) NOT NULL,
+    artist_id VARCHAR(18),
+    year INT,
+    duration DOUBLE PRECISION NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+    );
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR(18) PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    location VARCHAR(128),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION
+    );
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time TIME,
+    hour INT CHECK (hour >= 0 AND hour <= 23),
+    day INT CHECK (day >= 1 AND day <= 31),
+    week INT CHECK (week >= 1 AND week <= 52),
+    month INT CHECK (month >= 1 AND month <= 12),
+    year INT,
+    weekday INT CHECK (weekday >= 1 AND weekday <= 7)
+    );
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+
 """)
 
 user_table_insert = ("""
@@ -48,5 +94,5 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
